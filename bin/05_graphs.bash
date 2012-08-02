67b6ea50ae1e4705ff5b997e68a770499b04e1b9
Vince Forgetta <vince.forgetta@epi.jgh.mcgill.ca>
2012-08-02 09:21:10 -0400

    modified mhtplot, and top snps output

--- bin/05_graphs.bash
+++ bin/05_graphs.bash
@@ -5,7 +5,7 @@
 ## Merge data and produce graphs
 
 PROBABEL_DIR=$1
-PVALUE_CUTOFF="0.2"
+PVALUE_CUTOFF=$2
 
 cat > graphs.r << EOT
 library(GenABEL)
@@ -21,19 +21,22 @@ for(i in 2:length(files)) {
 }
 pvalue = pchisq((probabel_res\$beta_SNP_add/probabel_res\$sebeta_SNP_add)^2, df=1, lower=F)
 probabel_res = cbind(probabel_res, pvalue)
-write.table(probabel_res, file="probabel_res.txt")
+probabel_res = probabel_res[with(probabel_res, order(probabel_res\$chrom, probabel_res\$position)), ]
+write.table(probabel_res, file="probabel_res.txt", row.names=F, col.names=T, quote=F)
+
+png("mhtplot.png", width=10, height=13, unit="in", res=200)
+colors <- rep(c("blue", "green"),11)
+mhtplot(data.frame(probabel_res\$chrom, probabel_res\$position, probabel_res\$pvalue), control=mht.control(usepos=TRUE, srt=0, colors=colors),pch=19)
+axis(2,at=seq(0, max(-log10(probabel_res\$pvalue), na.rm=TRUE)), labels=seq(0, max(-log10(probabel_res\$pvalue), na.rm=TRUE)))
+dev.off()
+
 
 png(filename="qqplot.png")
 qqunif(probabel_res\$pvalue, pch=21, cex=.51, bg="black", bty="n", col="1", main=paste(c("QQ plot (", length(probabel_res\$pvalue), " SNPs)"), collapse=""))
 dev.off()
 
-png(filename="mhtplot.png")
-mhtplot(data.frame(probabel_res\$chrom, probabel_res\$position, probabel_res\$pvalue), xlab="SNP Position", ylab="-log10(p-value)", mht.control(cex=3, labels=probabel_res\$chrom, col=seq(1:22)), pch=".")
-dev.off()
-
 top_snps = subset(probabel_res, pvalue < ${PVALUE_CUTOFF})
-write.table(top_snps, file="top_snps.txt")
-
+write.table(top_snps, file="top_snps.txt", row.names=F, col.names=T, quote=F)
 EOT
 
 R --no-save < graphs.r
